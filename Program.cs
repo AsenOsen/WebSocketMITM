3c60ad5a36e321c7cedabf2a2dc4c9cfb8f3cd55
Anton <antochi.anton@yandex.ru>
2022-01-16 22:20:27 +0300

    Unknown class eliminated from example
    - removed SupremaMessageParser class

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@
 DnsPoisoner poisoner = new DnsPoisoner("127.0.0.1", new string[] { "google.com", "yahoo.com" });
 poisoner.poison();
 
-SupremaMessageParser suprema = new SupremaMessageParser();
 MitmServer mitmServer = new MitmServer();
 mitmServer.RequestCallback = delegate (string host, byte[] data) {
     Console.WriteLine("\n[Request]\n" + BitConverter.ToString(data)) ; 
